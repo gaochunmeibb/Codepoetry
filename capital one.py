412875a1eda721c67c1efd9c561b83dfd916df0f
gaochunmeibb <chunmeiGao@Chunmeis-MacBook-Pro.local>
2017-02-01 12:18:04 -0500

    add line

---
+++ capital one.py
@@ -0,0 +1,33 @@
+# def solveMeFirst(a,b):
+#    # Hint: Type return a+b below
+#   return (a+b)
+#
+#
+# num1 = input()
+# num2 = input()
+# res = solveMeFirst(num1,num2)
+# print res
+
+import sys
+import sys
+#!/bin/python
+
+
+#Sys.stdin.readline()
+
+
+
+#n = int(raw_input().strip())
+#arr = map(int,raw_input().strip().split(' '))
+
+import sys
+n=int(sys.stdin.readline().strip())
+arr = map(int,sys.stdin.readline().strip().split(' '))
+print n
+print arr
+# n = int(raw_input().strip())
+# print n
+#arr = map(int,raw_input().strip().split(' '))
+
+
+__author__ = 'chunmeiGao'
